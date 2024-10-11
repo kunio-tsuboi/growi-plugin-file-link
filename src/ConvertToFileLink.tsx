ad73795fa46346f4f5f948998ba4f092d5b121eb
邦夫 坪井 <kunio_tsuboi@unoudenshi.onmicrosoft.com>
2024-10-11 16:15:10 +0900

    release 1.0.2

--- src/ConvertToFileLink.tsx
+++ src/ConvertToFileLink.tsx
@@ -1,22 +1,21 @@
 export const convertToFileLink = (A: React.FunctionComponent<any>): React.FunctionComponent<any> => {
   return ({ children, href, ...props }) => {
     try {
-
-      const urlReg = /(https?:\/\/)([\w:%#\$&\?\(\)~\.=\+\-]+)?(\/?[\w/:%#\$&\?\(\)~\.=\+\-]+)/;
+      const urlReg = /(https?:\/\/)([\w:%#\$&\?\(\)~\.=\+\-]+)?(\/?.+)/;
       const backslashReg = /(\\|%5[cC])/g;
       const url = href.match(urlReg);
       if (url[2] !== undefined && url[2] == 'file.link') {
         // return the converted link
         const link = "file:/" + url[3].replace(backslashReg, '/');
-        return (<a href={link}>{children}</a>);
+        return (<a href={link} {...props}>{children}</a>);
       }
       // return the original
-      return (<A {...props}>{children}</A>);
+      return (<A href="${href}" {...props}>{children}</A>);
     }
     catch (err) {
       console.error(err);
     }
     // Return the original if an error occurs
-    return (<A {...props}>{children}</A>);
+    return (<A href="${href}" {...props}>{children}</A>);
   };
 };
